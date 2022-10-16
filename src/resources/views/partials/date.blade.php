f98b5f9f5c07a515342e01b064d2bfa9cbca071a
Moppa <moppa89@gmail.com>
2022-10-16 22:46:49 +0200

    Use negative days if in the past

--- src/resources/views/partials/date.blade.php
+++ src/resources/views/partials/date.blade.php
@@ -1,6 +1,6 @@
 @if (strtotime($datetime))
     <? $d = \Carbon\Carbon::parse($datetime) ?>
-    <span data-toggle="tooltip" title="{{ $d->diffInDays(\Carbon\Carbon::now(), false) }} days">{{ $d }}</span>
+    <span data-toggle="tooltip" title="{{ \Carbon\Carbon::now()->diffInDays($d, false) }} days">{{ $d }}</span>
 @else
     <span>{{ $datetime }}</span>
 @endif
